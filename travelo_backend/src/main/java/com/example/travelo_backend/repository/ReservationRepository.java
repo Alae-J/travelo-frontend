d96b70357591cec47646f3336dae6b51772f2ef1
Alae-J <alaejahid8@gmail.com>
2025-12-26 16:44:05 +0100

    Remove backend and restructure frontend repo

--- travelo_backend/src/main/java/com/example/travelo_backend/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travelo_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.travelo_backend.model.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> { 
-
-}
